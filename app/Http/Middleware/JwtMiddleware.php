da23187b1a2281c6b00267acdf20f20f387c2ea5
Kreyes2509 <118218673+Kreyes2509@users.noreply.github.com>
2023-08-07 14:11:32 -0600

    middleware JWT

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -4,6 +4,10 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Closure;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class JwtMiddleware
 {
