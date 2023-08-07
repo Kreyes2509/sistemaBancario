26003601f771709da83d42b349089fc5444f1a30
Kreyes2509 <118218673+Kreyes2509@users.noreply.github.com>
2023-08-07 05:51:59 -0600

    ruta login

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login',[AuthApiController::class,'loginApp']);
+
 Route::middleware(['jwt.verify'])->group(function () {
     Route::prefix('user')->group(function () {
         Route::get('/info',[AuthApiController::class,'getAuthenticatedUser']);
